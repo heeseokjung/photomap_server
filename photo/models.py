78ea41b438e29d3d04b373e5fd29421766e2faea
heeseokjung <heeseokjung@hanyang.ac.kr>
2021-01-21 23:49:31 -0800

    image path setting 2

--- photo/models.py
+++ photo/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Point(models.Model):
-    lattitude = models.CharField(max_length=30)
+    lattitude  = models.CharField(max_length=30)
     longtitude = models.CharField(max_length=30)
-    image = models.ImageField(upload_to='')
+    image      = models.ImageField(upload_to='%s' % self.__str__()) # lattitude와 longtitude를 to_string으로 바꿔서 정규표현식형태로 넣어야함
+
+    def __str__(self):
+        return lattitude + '/' + longtitude
