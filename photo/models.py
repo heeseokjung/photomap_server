a2c49ae69cd29a34bc0421d2be12674989598dce
heeseokjung <heeseokjung@hanyang.ac.kr>
2021-01-24 06:52:46 -0800

    ing

--- photo/models.py
+++ photo/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class Point(models.Model):
     lattitude  = models.CharField(max_length=30)
     longtitude = models.CharField(max_length=30)
-    image      = models.ImageField(upload_to='%s/%s' % (lattitude, longtitude)) # lattitude와 longtitude를 to_string으로 바꿔서 정규표현식형태로 넣어야함
+    image      = models.ImageField(upload_to="%s:%s" % (lattitude, longtitude))
