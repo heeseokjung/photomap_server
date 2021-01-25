b51bde4d6c12abc921d0562232a90318577406a3
heeseokjung <heeseokjung@hanyang.ac.kr>
2021-01-25 01:00:14 -0800

    model complete

--- photo/models.py
+++ photo/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
+def user_directory_path(instance, filename):
+    return '%s:%s/%s' % (instance.lattitude, instance.longtitude, filename)
+
 class Point(models.Model):
     lattitude  = models.CharField(max_length=30)
     longtitude = models.CharField(max_length=30)
-    image      = models.ImageField(upload_to="%s:%s" % (lattitude, longtitude))
+    image      = models.ImageField(upload_to=user_directory_path)
