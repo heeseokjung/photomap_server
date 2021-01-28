44993fac56f8b03beabc21c5e15eee7ff6641cf1
heeseokjung <heeseokjung@hanyang.ac.kr>
2021-01-27 23:59:26 -0800

    photo tmp

--- photo/views.py
+++ photo/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 def get(request):
     pass
 
 def post(request):
-    pass
+    if request.method == 'POST':
+        lattitude = request.POST['lattitude']
+        longtitude = request.POST['longtitude']
+        image = request.FILES['image']
+
+        p = Point(lattitude, longtitude, image)
+        p.save()
+
+        return JsonResponse({'message' : 'SUCCESS'}, status=200)
+    else:
+        return JsonResponse({'message' : 'INVALID KEY'}, status=400)
