233ed1c944e2102b7f3c4286e94102e486b5b88a
heeseokjung <heeseokjung@hanyang.ac.kr>
2021-02-11 02:40:50 -0800

    param

--- photo/views.py
+++ photo/views.py
@@ -9,14 +9,13 @@ def get(request):
 def post(request):
         
     if request.method == 'POST':
-        
         latitude = request.POST.get('latitude')
         longtitude = request.POST.get('longtitude')
         image = request.FILES.get('image')
         title = request.POST.get('title')
         content = request.POST.get('content')
 
-        p = Point(latitude, longtitude, title, content, image)
+        p = Point(latitude=latitude, longtitude=longtitude, title=title, content=content, image=image)
         p.save()
 
         return HttpResponse(status=200)
